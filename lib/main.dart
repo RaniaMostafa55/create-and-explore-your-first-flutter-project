5124bcf89701af6a418e9ba6a7c314e750eebc4e
RaniaMostafa55 <108219925+RaniaMostafa55@users.noreply.github.com>
2024-12-25 21:39:48 +0200

    add comments

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      //to remove debug sign
       debugShowCheckedModeBanner: false,
       title: 'The first flutter project',
       home: MyHomePage(),
@@ -26,18 +27,25 @@ class MyHomePage extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.red,
+        //put title of the page and style it
         title: const Text(
           "The first flutter project",
           style: TextStyle(color: Colors.white),
         ),
+        //centeralize title
         centerTitle: true,
       ),
+      //use column to put widgets vertically
       body: Column(
+        //centeralize widgets in the column
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //use "Row" to put widgets horizontally
           Row(
+            //let widgets take equal space in the row
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              //add two images from assets not from the internet
               Image.asset(
                 "assets/jerry.png",
                 width: MediaQuery.of(context).size.width * 0.15,
@@ -48,9 +56,11 @@ class MyHomePage extends StatelessWidget {
               ),
             ],
           ),
+          //create space between the row and the text
           SizedBox(
             height: MediaQuery.of(context).size.height * 0.03,
           ),
+          //use "Text" widget to add certain sentence and styling it
           const Text(
             "Tom & Jerry",
             style: TextStyle(
